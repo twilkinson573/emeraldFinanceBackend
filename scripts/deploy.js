b51339fdd5e5df0d67377f45f48e5ff75d1930c8
tuds <twilkinson573@gmail.com>
2021-12-22 15:01:16 +0000

    Add deploy script - working sample in Hardhat console

---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const main = async () => {
+  const [deployer] = await hre.ethers.getSigners();
+  const accountBalance = await deployer.getBalance();
+
+  console.log('Deploying contracts with account: ', deployer.address);
+  console.log('Account balance: ', accountBalance.toString());
+
+  const factory = await hre.ethers.getContractFactory('Greeter');
+  const portal = await factory.deploy("Scrah");
+  await portal.deployed();
+
+  console.log('Greeter address: ', portal.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
