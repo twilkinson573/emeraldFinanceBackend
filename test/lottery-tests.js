891a2ef540b7ecd7f331372f24ae3bcafcbeaefc
tuds <twilkinson573@gmail.com>
2022-01-06 16:22:32 +0000

    Add Lottery#makeDeposit initial test

--- test/lottery-tests.js
+++ test/lottery-tests.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Lottery", function () {
+describe("Lottery", () => {
 
   let deployer, bobAddr, emerald, lottery; 
   let totalEmeraldSupply = 100_000_000;
@@ -21,7 +21,7 @@ describe("Lottery", function () {
   });
 
 
-  it("Should own the total supply of EMER tokens", async function () {
+  it("Should own the total supply of EMER tokens", async () => {
     console.log(lottery.address)
     expect(await emerald.balanceOf(lottery.address)).to.equal(totalEmeraldSupply);
     expect(await emerald.balanceOf(emerald.address)).to.equal(0);
@@ -37,4 +37,22 @@ describe("Lottery", function () {
 
     // expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
+
+  describe("#makeDeposit", () => {
+
+    it("Should let the user deposit in the correct amount of USDC", async () => {
+
+    });
+
+
+    it("Should credit the user the correct number of EMER tokens", async () => {
+      depositTxn = await lottery.connect(bobAddr).makeDeposit(500);
+      await depositTxn.wait();
+
+      expect(await emerald.balanceOf(bobAddr.address)).to.equal(500);
+      expect(await emerald.balanceOf(lottery.address)).to.equal(totalEmeraldSupply - 500);
+    });
+
+  });
+
 });
